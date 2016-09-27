f82e6b6136210395e6e40baf387d81bd7f0e2253
Troy Sankey <sankeytms@gmail.com>
2016-09-26 22:44:05 -0400

    update backup script

--- hacks/backup_shifts.sh
+++ hacks/backup_shifts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date=$(date --iso)
-wget -O sbk_shifts_$date.csv http://calc.sankey.info/sbk_shifts.csv
-wget -O sbk_start_dates_$date.csv http://calc.sankey.info/sbk_start_dates.csv
+wget -O sgmc_shifts_$date.csv      http://calc.sankey.info/sgmc_shifts.csv
+wget -O sgmc_start_dates_$date.csv http://calc.sankey.info/sgmc_start_dates.csv
